37234b69a3c87c1ac8099f717141d28f438fd32c
Angel Coco Chen <angel@nuisance.co>
2018-05-02 22:19:20 +0800

    Exclude files from coverage report and test abstracted helper function

--- giftgiver/src/components/App.js
+++ giftgiver/src/components/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
 import { Button } from 'react-bootstrap'
 import Gift from './Gift'
+import { max_number } from '../helper'
+
 class App extends Component {
   state = {
     gifts: [],
   }
 
+  showGifts = () => {
+    const { gifts } = this.state
+    return gifts.map(gift => (
+      <Gift key={gift.id} gift={gift} removeGift={this.removeGift} />
+    ))
+  }
+
   addGift = () => {
     const { gifts } = this.state
 
-    const ids = gifts.map(gift => gift.id)
-
-    const maxId = ids.length > 0 ? Math.max(...ids) : 0
-    gifts.push({ id: maxId + 1 })
+    gifts.push({ id: max_number(gifts.map(gift => gift.id)) + 1 })
 
     this.setState({ gifts })
   }
@@ -28,11 +34,7 @@ class App extends Component {
     return (
       <div>
         <h2>Gift Giver</h2>
-        <div className="gift-list">
-          {gifts.map(gift => (
-            <Gift key={gift.id} gift={gift} removeGift={this.removeGift} />
-          ))}
-        </div>
+        <div className="gift-list">{this.showGifts()}</div>
         <Button className="btn-add" onClick={this.addGift}>
           Add Gift
         </Button>
