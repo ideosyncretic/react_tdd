ba65ea4ae637984beddfd72363c0f76c4cef03a8
Angel Coco Chen <angel@nuisance.co>
2018-05-09 21:58:41 +0800

    Add balance reducer and test

---
+++ lootcheck/src/reducers/balance.test.js
@@ -0,0 +1,16 @@
+import balanceReducer from './balance'
+import * as constants from '../actions/constants'
+
+describe('balanceReducer', () => {
+  it('sets a balance', () => {
+    const balance = 10
+
+    // balanceReducer is a pure function
+    expect(
+      balanceReducer(undefined, {
+        type: constants.SET_BALANCE,
+        balance,
+      }),
+    ).toEqual(balance)
+  })
+})
