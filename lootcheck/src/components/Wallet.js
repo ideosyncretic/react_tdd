485e5615aa6cc4af493562554f700becd9903714
Angel Coco Chen <angel@nuisance.co>
2018-05-09 22:22:17 +0800

    Add Wallet component and test

---
+++ lootcheck/src/components/Wallet.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+export class Wallet extends Component {
+  render() {
+    const { balance } = this.props
+    return (
+      <div>
+        <h3 className="balance">Wallet balance: {balance}</h3>
+      </div>
+    )
+  }
+}
+
+export default connect(state => {
+  balance: state
+}, null)(Wallet)
